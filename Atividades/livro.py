7a47b292edb889e886a4a79bb8f76e2280beb6d2
Nick e Gabi <nicollyghabryelle34@gmail.com>
2024-09-30 17:30:25 -0300

    Atividade de classes em python

---
+++ Atividades/livro.py
@@ -0,0 +1,20 @@
+class Livro: 
+    def __init__(self, titulo, autor, anoPublicacao):
+        self.titulo = titulo
+        self.autor = autor
+        self.anoPublicacao = anoPublicacao
+        
+    def exibirInformacao(self):
+        print(f"O título do livro é {self.titulo}, o(a) autor(a) é {self.autor} e o ano de publicação é {self.anoPublicacao}.")
+        
+    def verificarIdadeLivro(self):
+        if self.anoPublicacao < 1974:
+            print("Esse livro é um clássico.")
+        else:
+            print("Ainda não é considerado um clássico")
+    
+
+l1 = Livro("Pequeno Príncipe","Antoine de Saint-Exupéry",1943)
+l1.exibirInformacao()
+
+l1.verificarIdadeLivro() 
---
+++ Atividades/principal.py
@@ -0,0 +1,9 @@
+from livro import Livro
+
+l2 = Livro("A biblioteca da meia noite","Matt Haig",2020)
+l2.exibirInformacao()
+l2.verificarIdadeLivro() 
+
+l2 = Livro("O Diabo Veste Prada","Lauren Weisberger",2003)
+l2.exibirInformacao()
+l2.verificarIdadeLivro() 
