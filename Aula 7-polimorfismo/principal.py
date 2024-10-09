10ba032852031c526f0e9329ef6ff1b54a0430b7
Nick e Gabi <nicollyghabryelle34@gmail.com>
2024-10-09 15:01:26 -0300

    Trabalhando com Polimorfismo em Python

---
+++ Aula 7-polimorfismo/principal.py
@@ -0,0 +1,9 @@
+from produto import Produto, Eletronico
+
+prod1 = Produto("Armário", 758.99)
+prod1.descrever()
+prod1.calcularDesconto(10)
+
+eletro1 = Eletronico("Smartphone", 1544.99, 220)
+eletro1.descrever()
+eletro1.calcularDesconto(30)
